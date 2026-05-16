3ae3f9b68dd1d39d948353713eaf9132c8d51a7a
Akshay Bhagat <akshaybhagat28@gmail.com>
2026-05-16 13:19:47 +0530

    attendacetrackingapp

--- AttendanceTrackService/src/main/java/com/nexusHr/attendanceTrackService/AttendanceTrackingServiceApplication.java
+++ AttendanceTrackService/src/main/java/com/nexusHr/attendanceTrackService/AttendanceTrackingServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class AttendanceTrackingServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("AttendanceTrackingServiceApplication");
 		SpringApplication.run(AttendanceTrackingServiceApplication.class, args);
 	}
 }
